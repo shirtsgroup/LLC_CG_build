2064e4e8c4f8b6d2168355ee6fde688bbde35ddd
subinsahu <subinsahu@gmail.com>
2021-11-10 13:10:38 -0700

    1fs timestep during minimization

--- gyroid/L9.2/mon80_gly20/New/1_minimize/minimize.sh
+++ gyroid/L9.2/mon80_gly20/New/1_minimize/minimize.sh
@@ -15,11 +15,7 @@ sed -i "s/repl_itr/$i/g" min$i.mdp
 gmx grompp -f min$i.mdp -c initial.gro -p topol.top -r initial.gro -o min$i -maxwarn 5
 gmx mdrun -deffnm min$i
 
-:<<EOF
-
-EOF
-
-for i in `seq 1 8`
+for i in `seq 1 9`
 do
     j=$((i-1))
     cp -f em_scaled.mdp min$i.mdp
@@ -28,7 +24,7 @@ do
     gmx mdrun -v --deffnm min$i
 done
 
-gmx grompp -f sd.mdp -c min8.gro -p topol.top -r initial.gro -o sd -maxwarn 5
+gmx grompp -f sd.mdp -c min9.gro -p topol.top -r initial.gro -o sd -maxwarn 5
 gmx mdrun -deffnm sd
 
 #clean
@@ -36,4 +32,3 @@ mkdir -p intermidiates
 mv -f min?.* intermidiates
 
 
-#solvate
